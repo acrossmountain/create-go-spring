a01dd6f2cfcd9db1ff514e64a8a1a8f53f842aff
Qyq <18339795172@163.com>
2021-05-14 09:22:10 +0800

    使用 github.com/urfave/cli/v2

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/acrossmountain/create-go-spring
 
 go 1.14
 
-require github.com/go-spring/go-spring-parent v1.0.5-0.20200630104937-81458ffbf0a5
+require github.com/urfave/cli/v2 v2.3.0
--- main.go
+++ main.go
@@ -1,18 +1,26 @@
 package main
 
 import (
-	"time"
+	"log"
+	"os"
 
-	"github.com/go-spring/go-spring-parent/spring-logger"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	SpringLogger.Info("create-go-spring => " + time.Now().Format("2006-01-02 15:04:05"))
 	// 1. 获取命令行参数
 	// 1.1 设置提示用法
 	// 1.2 判断是否有参数传入
 	// 1.3 显示提示帮助
 	// 2. 获取所有参数
 	// 2.1 获取详细命令
-	
+	app := &cli.App{
+		Name:  "create-go-spring",
+		Usage: "create-go-spring -m 'github.com/go-spring/create-go-spring' [ web http-rpc redis gorm ... ]",
+	}
+	err := app.Run(os.Args)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
